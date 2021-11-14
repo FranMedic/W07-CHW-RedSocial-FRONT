b1f357c594a6087ebe78e2e341d1745cfcfb40db
Franny Medico <m.francisca.medico@gmail.com>
2021-11-14 19:16:45 +0100

    added login form page and customHook

---
+++ src/pages/LoginForm/LoginForm.js
@@ -0,0 +1,99 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import useUser from "../../hooks/useUser";
+
+const LoginForm = () => {
+  const { login } = useUser();
+  let navigate = useNavigate();
+  const initalValues = {
+    username: "",
+    password: "",
+  };
+
+  const [userData, setUserData] = useState(initalValues);
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [wrongCredentials, setWrongCredentials] = useState(false);
+
+  const onChangeData = (event) => {
+    setUserData({ ...userData, [event.target.id]: event.target.value });
+  };
+
+  const onLogin = async (event) => {
+    setWrongCredentials(false);
+    event.preventDefault();
+    const response = await login(userData);
+    if (!response) {
+      navigate("/");
+    } else {
+      setWrongCredentials(true);
+    }
+    setUserData(initalValues);
+  };
+
+  useEffect(() => {
+    if (userData.username !== "" && userData.password !== "") {
+      setIsDisabled(false);
+    } else {
+      setIsDisabled(true);
+    }
+  }, [userData.password, userData.username]);
+
+  return (
+    <>
+      <div className="container ">
+        <section className="row justify-content-center">
+          <div className=" col-4 form-container">
+            <form autoComplete="off" onSubmit={onLogin} noValidate>
+              <div className="mb-3 row">
+                <label htmlFor="username" className="form-label">
+                  Username
+                </label>
+                <input
+                  type="text"
+                  id="username"
+                  placeholder="UserName"
+                  value={userData.username}
+                  onChange={onChangeData}
+                  required
+                  className="mb-2 form-control"
+                />
+              </div>
+              <div className="mb-3 row">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+
+                <input
+                  type="password"
+                  value={userData.password}
+                  placeholder="Password"
+                  onChange={onChangeData}
+                  required
+                  className="form-control"
+                  id="password"
+                />
+              </div>
+
+              <button
+                value="Submit"
+                className="btn btn-primary mt-1 mb-5"
+                type="submit"
+                disabled={isDisabled}
+              >
+                Login
+              </button>
+              <Link to="/signup">
+                <button className="register-button">
+                  Don't have an account? Sign up!
+                </button>
+              </Link>
+            </form>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default LoginForm;
