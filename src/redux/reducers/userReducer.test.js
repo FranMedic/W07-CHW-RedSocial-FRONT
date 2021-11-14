3e525a36be78e0dfb36500d852f64beab0895b73
Franny Medico <m.francisca.medico@gmail.com>
2021-11-14 17:39:57 +0100

    added tests of userReducer

---
+++ src/redux/reducers/userReducer.test.js
@@ -0,0 +1,53 @@
+import { userLoginAction, userLogoutAction } from "../actions/actionCreators";
+import actionTypes from "../actions/actionTypes";
+import userReducer from "./userReducer";
+
+describe("Given a userReducer reducer", () => {
+  describe("when it receives a user object with a property isAuthenticated as false and a list of user empty", () => {
+    describe("and a login action with a user", () => {
+      test("then it should return an new user list with the authenticated as true", () => {
+        const userFake = {
+          name: "Franny",
+          username: "frannyLove",
+          password: "frannylovesyou",
+          photo: "patata",
+          enemies: [],
+          friends: [],
+          bio: "Holi bebe! ",
+        };
+        const action = userLoginAction(userFake);
+        const expecteduser = {
+          isAuthenticated: true,
+          user: userFake,
+        };
+
+        const newUser = userReducer(userFake, action);
+
+        expect(newUser).toEqual(expecteduser);
+      });
+    });
+
+    describe("and a logout action", () => {
+      test("then it should return a new user with an empty object and a property isAuthenticated as false", () => {
+        const userFake = {
+          name: "Franny",
+          username: "frannyLove",
+          password: "frannylovesyou",
+          photo: "patata",
+          enemies: [],
+          friends: [],
+          bio: "Holi bebe! ",
+        };
+        const action = userLogoutAction();
+        const expecteduser = {
+          user: {},
+          isAuthenticated: false,
+        };
+
+        const newUser = userReducer(userFake, action);
+
+        expect(newUser).toEqual(expecteduser);
+      });
+    });
+  });
+});
