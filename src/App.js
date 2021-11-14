6df5ac0a85243f964df7e42113aa7f22f5349215
Franny Medico <m.francisca.medico@gmail.com>
2021-11-14 19:17:09 +0100

    added routes in app and logout call

--- src/App.js
+++ src/App.js
@@ -1,7 +1,48 @@
+import { Route, Router, Routes } from "react-router";
+import { Link } from "react-router-dom";
 import "./App.css";
+import useUser from "./hooks/useUser";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginForm from "./pages/LoginForm/LoginForm";
 
 function App() {
-  return;
+  const { user, logout } = useUser();
+
+  const onLogout = (event) => {
+    event.preventDefault();
+    logout();
+    localStorage.removeItem(process.env.REACT_APP_LOCAL_STORAGE);
+  };
+  return (
+    <>
+      <Router>
+        <header className="header">
+          <Link to="/">
+            <h1 className="title">FrannyGram</h1>
+          </Link>
+          {user.isAuthenticated ? (
+            <div>
+              <button
+                className="logout-button btn btn-primary mt-1 mb-5"
+                onClick={onLogout}
+              >
+                Log Out
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
+        </header>
+        <Routes>
+          <Route
+            path="/*"
+            element={user.isAuthenticated ? <HomePage /> : <LoginForm />}
+          />
+          <Route path="/login" element={<LoginForm />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
