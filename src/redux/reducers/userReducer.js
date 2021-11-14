5ce14c90f172149d0fbd6421b1bc32becddb2c81
Franny Medico <m.francisca.medico@gmail.com>
2021-11-14 16:22:07 +0100

    added user reducer for userlogin and userLogout

---
+++ src/redux/reducers/userReducer.js
@@ -0,0 +1,23 @@
+import actionTypes from "../actions/actionTypes";
+
+const userReducer = (user = { isAuthenticated: false, user: {} }, action) => {
+  let newUser;
+
+  switch (action.type) {
+    case actionTypes.userLogin:
+      newUser = {
+        isAuthenticated: true,
+        user: {},
+      };
+      break;
+    case actionTypes.userLogout:
+      newUser = { isAuthenticated: false, user: {} };
+      break;
+
+    default:
+      newUser = user;
+  }
+  return newUser;
+};
+
+export default userReducer;
