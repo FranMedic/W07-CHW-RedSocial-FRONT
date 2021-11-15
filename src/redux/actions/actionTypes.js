3ef514ab5b6674f08f14133a5b9b98e4e2bc0bdc
Francisca Medico <87529879+FranMedic@users.noreply.github.com>
2021-11-15 19:15:41 +0100

    Revert "Feature/user list reducer"

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -2,6 +2,5 @@ const actionTypes = {
   userLogin: "USER_LOGIN",
   userLogout: "USER_LOGOUT",
   userRegister: "USER_REGISTER",
-  loadUsers: "LOAD_USERS",
 };
 export default actionTypes;
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
-import userListReducer from "./userListReducer";
 import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
-  userList: userListReducer,
 });
 
 export default rootReducer;
