bf73365c0d5e1764c2703c89a188318a9563bd54
Franny Medico <m.francisca.medico@gmail.com>
2021-11-15 17:40:32 +0100

    added laod users type and creator

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import userListReducer from "./userListReducer";
 import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
+  userList: userListReducer,
 });
 
 export default rootReducer;
